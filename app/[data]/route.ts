2f46125ce86bb84518b07fe50b59d10b7db3e02a
davisbento <contato@davisbento.dev>
2023-12-31 18:21:04 -0300

    serve roadmaps

---
+++ app/[data]/route.ts
@@ -0,0 +1,26 @@
+import { promises as fs } from 'fs';
+
+type Params = {
+	params: {
+		data: string;
+	};
+};
+
+export async function GET(req: Request, { params }: Params) {
+	const dataName = params.data;
+
+	const path = process.cwd() + '/app/' + dataName + '.json';
+	try {
+		const file = await fs.readFile(path, 'utf8');
+
+		const data = JSON.parse(file);
+
+		return Response.json({
+			data: data
+		});
+	} catch (err) {
+		return Response.json({
+			error: 'File not found'
+		});
+	}
+}
